ce1d48748ec971c24f30b395f7155f517851d5ac
Milthon <milthonbor@gmail.com>
2026-01-20 16:37:47 -0700

    creacion de patron abtracto y archivos de patrones a trabjar

---
+++ creational/abstract_creational.py
@@ -0,0 +1,80 @@
+
+
+"""
+Metodos de pago con una estrucura bien definida para cada unao de los metodos de pago que se requiera adquier 
+"""
+
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+class Payment(ABC):
+    
+
+    @abstractmethod
+    def pay(self, amount:float, reference:str) -> str:
+        ...
+    
+    @abstractmethod
+    def get_information_pay_by_id(self, pay_id:int) -> str:
+        ...
+    
+    @abstractmethod
+    def get_history_by_reference_pay(self, reference:str) -> str:
+        ...
+
+
+
+
+class MercadoPagoPayment(Payment):
+
+    def pay(self, amount:float, reference:str) -> str:
+        return f"Se pago en MercadoPago - cantidad: {amount} -  a la referencia: {reference}"
+    
+    
+    def get_information_pay_by_id(self, pay_id:int) -> str:
+        return f"El pago con al referencia:MERCA123 -> se realizo enl dia: {datetime.now().isoformat()}"
+        
+    
+    def get_history_by_reference_pay(self, reference:str) -> str:
+        return f"Hisotira de la referencia: {reference} - movimeintos 10 en las ultamas 24 horas"
+
+
+class PayPalPaymet(Payment):
+
+    def pay(self, amount:float, reference:str) -> str:
+        return f"Se pago en Paypal - cantidad: {amount} -  a la referencia: {reference}"
+    
+    
+    def get_information_pay_by_id(self, pay_id:int) -> str:
+        return f"El pago con al referencia:PAYOAL123 -> se realizo enl dia: {datetime.now().isoformat()}"
+        
+    
+    def get_history_by_reference_pay(self, reference:str) -> str:
+        return f"Hisotira de la referencia: {reference} - movimeintos 10 en las ultamas 24 horas"
+
+
+
+        
+def pay_account_imp(pago:Payment, amount:float, reference:str) -> None:
+
+    print(pago.pay(amount=amount, reference=reference))
+    print(pago.get_information_pay_by_id(pay_id=123))
+    print(pago.get_history_by_reference_pay(reference=reference))
+
+
+        
+
+if __name__ == "__main__":
+
+    mercado: Payment = MercadoPagoPayment()
+
+    print("Procesando pago en Mercado Pago")
+    pay_account_imp(pago=mercado, amount=150.34, reference="mercado12334")
+
+    paypal: Payment = PayPalPaymet()
+
+    print("Procesando pago en Paypal")
+    pay_account_imp(pago=paypal, amount=7781.34, reference="paypal2124")
+
+
+
